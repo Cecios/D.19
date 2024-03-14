42210e9b3e46c579cb298e764980da07591d3f07
Andrea <andrea.raneri94@gmail.com>
2024-03-14 01:29:17 +0100

    1° route

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router();
 const UsersModel = require('../models/users')
 
-
+router.get('/authors', async (request, response)=>{})
 
 module.exports = router;
